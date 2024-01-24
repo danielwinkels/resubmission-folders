d718d1f1c0dc6fc47c0e0952ce2d8751e2a3c618
Daniel Winkels <Geuer_57@Geuer.local>
2024-01-24 10:24:15 +0100

    Erstellen eines Moduls

--- PowerShell/ResubmissionFolder-Module.psm1
+++ PowerShell/ResubmissionFolder-Module.psm1
@@ -1,3 +1,22 @@
+function New-ResubmissionFolders {
+    param (
+        [Parameter(Mandatory=$true, Position=0, HelpMessage="Specify the start date.")]
+        [datetime]$FromDate,
+
+        [Parameter(Mandatory=$true, Position=1, HelpMessage="Specify the end date.")]
+        [datetime]$UntilDate,
+
+        [Parameter(Position=2, HelpMessage="Specify the start directory.")]
+        [string]$StartDirectory = (Get-Location).Path
+    )
+
+    for ($date = $FromDate; $date -le $UntilDate; $date = $date.AddDays(1)) {
+        $folderPath = Get-DateFolderPath -Date $date -StartDirectory $StartDirectory
+        Write-Host "Datum: $date - Ordnerpfad: $folderPath"
+        New-Item -ItemType Directory -Path $folderPath -Force
+    }
+}
+
 # Wähle zwischen den beiden Funktionen Get-DateFolderPathDayOnly und Get-DateFolderPathFullDate
 Function Get-DateFolderPath {
     param (
